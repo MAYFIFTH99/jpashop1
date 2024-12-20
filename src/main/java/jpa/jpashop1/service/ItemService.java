b4aa21d6ea0f0499e3c6fa78f04cb9cf758570f0
harvey <alstjr971@naver.com>
2024-12-21 00:54:31 +0900

    feat : 상품 수정(변경 감지)

--- src/main/java/jpa/jpashop1/service/ItemService.java
+++ src/main/java/jpa/jpashop1/service/ItemService.java
@@ -29,5 +29,15 @@ public class ItemService {
         return itemRepository.findById(id);
     }
 
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int quantity) {
+        Item item = itemRepository.findById(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(quantity);
+        // 영속성 컨텍스트에 존재하는 엔티티를 추출한 후 , 그 값을 수정함으로써
+        // 변경 감지를 이용해야 하지, 엔티티를 어설프게 수정하는 것은 매우 위험하다.
+    }
+
 
 }
