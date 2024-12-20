56cd9c95143073eb10c268a3aed1c1d4a1bde87d
harvey <alstjr971@naver.com>
2024-12-20 11:11:31 +0900

    Chore : add p6spy

--- src/test/java/jpa/jpashop1/MemberRepositoryTest.java
+++ src/test/java/jpa/jpashop1/MemberRepositoryTest.java
@@ -3,12 +3,14 @@ package jpa.jpashop1;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberRepositoryTest {
 
     @Autowired
