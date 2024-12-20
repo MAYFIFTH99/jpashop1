23f6fe7ed6acae75d54f442be8148193ede08b6a
harvey <alstjr971@naver.com>
2024-12-20 22:22:44 +0900

    feat : 회원 등록 및 조회 기능

--- src/main/java/jpa/jpashop1/controller/MemberForm.java
+++ src/main/java/jpa/jpashop1/controller/MemberForm.java
@@ -1,2 +1,17 @@
-package jpa.jpashop1.controller;public class MemberForm {
+package jpa.jpashop1.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotEmpty(message ="필수로 입력해야 합니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
 }
