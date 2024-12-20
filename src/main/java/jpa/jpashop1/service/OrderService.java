d07c8621cc3e2423399a18c4d9bde00933a49a8e
harvey <alstjr971@naver.com>
2024-12-20 15:37:46 +0900

    feat : 주문 서비스

--- src/main/java/jpa/jpashop1/service/OrderService.java
+++ src/main/java/jpa/jpashop1/service/OrderService.java
@@ -1,2 +1,59 @@
-package jpa.jpashop1.service;public class OrderService {
+package jpa.jpashop1.service;
+
+import jpa.jpashop1.domain.Delivery;
+import jpa.jpashop1.domain.Member;
+import jpa.jpashop1.domain.Order;
+import jpa.jpashop1.domain.OrderItem;
+import jpa.jpashop1.domain.item.Item;
+import jpa.jpashop1.repository.ItemRepository;
+import jpa.jpashop1.repository.MemberRepository;
+import jpa.jpashop1.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+    private final DataSourceTransactionManager transactionManager;
+
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        Member member = memberRepository.findById(memberId);
+        Item item = itemRepository.findById(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+    //검색
+//    public List<Order> findOrders(OrderSearchCond cond){}
+
 }
