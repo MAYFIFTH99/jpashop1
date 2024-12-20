5d2083fd518fae301b92a78553595a7bbcecd992
harvey <alstjr971@naver.com>
2024-12-20 11:02:31 +0900

    Docs : first setting

---
+++ src/test/java/jpa/jpashop1/MemberRepositoryTest.java
@@ -0,0 +1,29 @@
+package jpa.jpashop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        Long savedId = memberRepository.save(member);
+
+        //when
+        Member findMember = memberRepository.find(savedId);
+        //then
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
