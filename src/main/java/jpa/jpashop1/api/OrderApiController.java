a1effd570b978bd8f17520d241f0edcedb60b8e7
harvey <alstjr971@naver.com>
2024-12-21 23:03:04 +0900

    refactor : v3 -> v3.1 : 페이징 한계 극복 (BatchSize 활용)

--- src/main/java/jpa/jpashop1/api/OrderApiController.java
+++ src/main/java/jpa/jpashop1/api/OrderApiController.java
@@ -5,6 +5,7 @@ import jpa.jpashop1.repository.OrderRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -42,6 +43,15 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).toList();
+        return result;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_Pageable(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).toList();
         return result;
     }
--- src/main/java/jpa/jpashop1/domain/Order.java
+++ src/main/java/jpa/jpashop1/domain/Order.java
@@ -3,6 +3,7 @@ package jpa.jpashop1.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @BatchSize(size = 1000)
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
