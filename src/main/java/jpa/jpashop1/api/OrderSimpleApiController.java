f5ea598ce6db32400a3f8a46072fc6da7bb6c973
harvey <alstjr971@naver.com>
2024-12-21 16:19:25 +0900

    refactor : v2 N+1 문제 발생

--- src/main/java/jpa/jpashop1/api/OrderSimpleApiController.java
+++ src/main/java/jpa/jpashop1/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpa.jpashop1.api;
 
 
+import jpa.jpashop1.domain.Address;
 import jpa.jpashop1.domain.Order;
 import jpa.jpashop1.domain.OrderSearch;
+import jpa.jpashop1.domain.OrderStatus;
 import jpa.jpashop1.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -26,5 +31,29 @@ public class OrderSimpleApiController {
     public List<Order> orddersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> orderDtos = orders.stream().map(m -> new SimpleOrderDto(m.getId(), m.getMember().getName(), m.getOrderDate(), m.getStatus(), m.getDelivery().getAddress())).toList();
+        return new Result(orderDtos);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime localDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
     }
 }
